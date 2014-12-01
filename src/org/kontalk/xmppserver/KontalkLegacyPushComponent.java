064d92e27beb9a3ab6ac654319e7c3f60d9bd7f5
Daniele Ricci <daniele.athome@gmail.com>
2014-12-01 23:16:50 +0100

    Very basic and legacy Kontalk push notifications component
    Signed-off-by: Daniele Ricci <daniele.athome@gmail.com>

---
+++ src/org/kontalk/xmppserver/KontalkLegacyPushComponent.java
@@ -0,0 +1,108 @@
+package org.kontalk.xmppserver;
+
+import tigase.conf.ConfigurationException;
+import tigase.server.AbstractMessageReceiver;
+import tigase.server.Iq;
+import tigase.server.Message;
+import tigase.server.Packet;
+import tigase.xml.Element;
+import tigase.xmpp.JID;
+
+import java.util.*;
+
+
+/**
+ * Kontalk legacy push component.
+ * Supports only GCM.
+ * @author Daniele Ricci
+ */
+public class KontalkLegacyPushComponent extends AbstractMessageReceiver {
+
+    private static final String DISCO_DESCRIPTION = "Legacy Kontalk push notifications";
+    private static final String NODE = "http://kontalk.org/extensions/presence#push";
+    private static final String XMLNS = NODE;
+    private static final Element top_feature = new Element("feature", new String[] { "var" },  new String[] { NODE });
+    private static final List<Element> DISCO_FEATURES = Arrays.asList(new Element[] { top_feature });
+
+    public static final String GCM_PROVIDER_NAME = "gcm";
+    private static final String GCM_JID_PREFIX = "gcm.push.";
+    private static final String GCM_DESCRIPTION = "Google Cloud Messaging push notifications";
+
+    private String gcmProjectId;
+    private String gcmApiKey;
+
+    private Map<String, String> storage = new HashMap<String, String>(100);
+
+    @Override
+    public void processPacket(Packet packet) {
+        if (packet.getElemName().equals(Iq.ELEM_NAME)) {
+            Element register = packet.getElement().getChild("register", XMLNS);
+            if (register != null && GCM_PROVIDER_NAME.equals(register.getAttributeStaticStr("provider"))) {
+                String regId = register.getCData();
+                if (regId != null && regId.length() > 0) {
+                    storage.put(packet.getStanzaFrom().getBareJID().toString(), regId);
+                }
+            }
+        }
+
+        else if (packet.getElemName().equals(Message.ELEM_NAME)) {
+            if ("push".equals(packet.getAttributeStaticStr("type")) && packet
+                    .getStanzaFrom().toString().equals(getDefVHostItem().getDomain())) {
+
+                Element push = packet.getElement().getChild("push", XMLNS);
+                if (push != null) {
+                    String jid = push.getAttributeStaticStr("jid");
+                    if (jid != null && jid.length() > 0) {
+                        System.out.println("sending notification to " + jid);
+                        // TODO send push notification
+                    }
+                }
+
+            }
+
+        }
+    }
+
+    @Override
+    public Map<String, Object> getDefaults(Map<String, Object> params) {
+        Map<String, Object> defs = super.getDefaults(params);
+        defs.put("packet-types", new String[]{"iq"});
+        return defs;
+    }
+
+    @Override
+    public void setProperties(Map<String, Object> props) throws ConfigurationException {
+        super.setProperties(props);
+
+        gcmProjectId = (String) props.get("gcm-projectid");
+        gcmApiKey = (String) props.get("gcm-apikey");
+    }
+
+    @Override
+    public String getDiscoDescription() {
+        return DISCO_DESCRIPTION;
+    }
+
+    @Override
+    public String getDiscoCategoryType() {
+        return "generic";
+    }
+
+    @Override
+    public List<Element> getDiscoItems(String node, JID jid, JID from) {
+        if (NODE.equals(node)) {
+            List<Element> list = new ArrayList<Element>(1);
+            list.add(new Element("item", new String[]{"node", "jid", "name"},
+                    new String[]{ gcmProjectId, GCM_JID_PREFIX + getDefVHostItem().getDomain(), GCM_DESCRIPTION }));
+            return list;
+        }
+
+        return null;
+    }
+
+    @Override
+    public List<Element> getDiscoFeatures(JID from) {
+        return DISCO_FEATURES;
+    }
+
+}
---
+++ src/org/kontalk/xmppserver/KontalkPushNotifications.java
@@ -0,0 +1,130 @@
+package org.kontalk.xmppserver;
+
+
+import tigase.db.NonAuthUserRepository;
+import tigase.db.TigaseDBException;
+import tigase.server.Iq;
+import tigase.server.Message;
+import tigase.server.Packet;
+import tigase.server.Presence;
+import tigase.util.TigaseStringprepException;
+import tigase.xml.Element;
+import tigase.xmpp.*;
+import tigase.xmpp.impl.roster.RosterAbstract;
+import tigase.xmpp.impl.roster.RosterFactory;
+
+import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import static tigase.xmpp.impl.roster.RosterAbstract.SUB_BOTH;
+import static tigase.xmpp.impl.roster.RosterAbstract.SUB_TO;
+
+
+/**
+ * Kontalk legacy push notifications implementation.
+ * Support for GCM only. The hard-coded stuff.
+ * @author Daniele Ricci
+ */
+public class KontalkPushNotifications extends XMPPProcessor implements XMPPPostprocessorIfc {
+
+    private static Logger log = Logger.getLogger(KontalkPushNotifications.class.getName());
+    public static final String XMLNS = "http://kontalk.org/extensions/presence#push";
+    public static final String ID = "kontalk:push:legacy";
+
+    private static final String[] PRESENCE_CAPS_PATH = { Presence.ELEM_NAME, "c" };
+    private static final String[][] ELEMENTS = { PRESENCE_CAPS_PATH, { Message.ELEM_NAME } };
+    private static final String[] XMLNSS = {XMLNS, Message.CLIENT_XMLNS};
+
+    private String componentName;
+
+    @Override
+    public void init(Map<String, Object> settings) throws TigaseDBException {
+        componentName = (String) settings.get("component");
+        if (componentName == null)
+            componentName = "push";
+    }
+
+    @Override
+    public void postProcess(Packet packet, XMPPResourceConnection session, NonAuthUserRepository repo, Queue<Packet> results, Map<String, Object> settings) {
+        if (log.isLoggable(Level.FINEST)) {
+            log.finest("Processing packet: " + packet.toString());
+        }
+
+        if (packet.getElemName().equals(Presence.ELEM_NAME)) {
+            if (session == null) {
+                if (log.isLoggable( Level.FINE)) {
+                    log.log( Level.FINE, "Session is null, ignoring packet: {0}", packet );
+                }
+                return;
+            }
+            if (!session.isAuthorized()) {
+                if ( log.isLoggable( Level.FINE ) ){
+                    log.log( Level.FINE, "Session is not authorized, ignoring packet: {0}", packet );
+                }
+                return;
+            }
+
+            try {
+
+                Element element = packet.getElement();
+                Element cap = element.getChild("c", XMLNS);
+                if (cap != null && KontalkLegacyPushComponent.GCM_PROVIDER_NAME.equals(cap.getAttributeStaticStr("provider"))) {
+                    String regId = cap.getCData();
+                    if (regId != null && regId.length() > 0) {
+                        // create registration request
+                        Element request = new Element("iq");
+                        request.setAttribute("type", "set");
+
+                        request.addChild(new Element("register",
+                                regId,
+                                new String[]{"xmlns", "provider"},
+                                new String[]{XMLNS, KontalkLegacyPushComponent.GCM_PROVIDER_NAME}));
+
+                        // send regId to push component
+                        JID compJid = JID.jidInstanceNS(componentName, session.getDomainAsJID().getDomain(), null);
+                        Packet p = Packet.packetInstance(request, session.getJID(), compJid);
+                        results.offer(p);
+                    }
+                }
+            } catch (NotAuthorizedException e) {
+                // TODO
+                log.log(Level.WARNING, "Not authorized.");
+            }
+        }
+
+        else if (packet.getElemName().equals(Message.ELEM_NAME)) {
+            if (session == null) {
+                // create registration request
+                Element request = new Element("message");
+                request.setAttribute("type", "push");
+
+                request.addChild(new Element("push",
+                        new String[]{"xmlns", "jid"},
+                        new String[]{XMLNS, packet.getStanzaTo().getBareJID().toString()}));
+
+                // send regId to push component
+                JID compJid = JID.jidInstanceNS(componentName, packet.getStanzaTo().getDomain(), null);
+                JID fromJid = JID.jidInstanceNS(packet.getStanzaTo().getDomain());
+                Packet p = Packet.packetInstance(request, fromJid, compJid);
+                results.offer(p);
+            }
+        }
+    }
+
+    @Override
+    public String id() {
+        return ID;
+    }
+
+    @Override
+    public String[][] supElementNamePaths() {
+        return ELEMENTS;
+    }
+
+    @Override
+    public String[] supNamespaces() {
+        return XMLNSS;
+    }
+
+}

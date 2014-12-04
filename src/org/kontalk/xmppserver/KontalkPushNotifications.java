5600373f3ffe06c00d1ccf2e65aaa8a64669602c
Daniele Ricci <daniele.athome@gmail.com>
2014-12-04 21:54:10 +0100

    Remove offline crap
    Signed-off-by: Daniele Ricci <daniele.athome@gmail.com>

--- src/org/kontalk/xmppserver/KontalkPushNotifications.java
+++ src/org/kontalk/xmppserver/KontalkPushNotifications.java
@@ -1,24 +1,16 @@
 package org.kontalk.xmppserver;
 
-import java.util.Date;
+import tigase.db.NonAuthUserRepository;
+import tigase.db.TigaseDBException;
+import tigase.server.Message;
+import tigase.server.Packet;
+import tigase.xml.Element;
+import tigase.xmpp.*;
+
 import java.util.Map;
 import java.util.Queue;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
-import tigase.db.MsgRepositoryIfc;
-import tigase.db.NonAuthUserRepository;
-import tigase.db.TigaseDBException;
-import tigase.db.UserNotFoundException;
-import tigase.server.Message;
-import tigase.server.Packet;
-import tigase.xml.Element;
-import tigase.xmpp.JID;
-import tigase.xmpp.StanzaType;
-import tigase.xmpp.XMPPPostprocessorIfc;
-import tigase.xmpp.XMPPProcessor;
-import tigase.xmpp.XMPPResourceConnection;
-import tigase.xmpp.impl.OfflineMessages;
 
 
 /**
@@ -37,8 +29,6 @@ public class KontalkPushNotifications extends XMPPProcessor implements XMPPPostp
 
     private String componentName;
 
-    private OfflineMessages offlineProcessor = new OfflineMessages();
-
     @Override
     public void init(Map<String, Object> settings) throws TigaseDBException {
         componentName = (String) settings.get("component");
@@ -55,7 +45,7 @@ public class KontalkPushNotifications extends XMPPProcessor implements XMPPPostp
         if (session == null && packet.getElemName().equals(Message.ELEM_NAME) &&
             packet.getType() == StanzaType.chat && packet.getElement().getChild("body") != null) {
 
-            // create registration request
+            // create push notification message
             Element request = new Element("message");
             request.setAttribute("type", "push");
 
@@ -63,23 +53,11 @@ public class KontalkPushNotifications extends XMPPProcessor implements XMPPPostp
                     new String[]{"xmlns", "jid"},
                     new String[]{XMLNS, packet.getStanzaTo().getBareJID().toString()}));
 
-            // send regId to push component
+            // send request to push component
             JID compJid = JID.jidInstanceNS(componentName, packet.getStanzaTo().getDomain(), null);
             JID fromJid = JID.jidInstanceNS(packet.getStanzaTo().getDomain());
             Packet p = Packet.packetInstance(request, fromJid, compJid);
             results.offer(p);
-
-            // save message to offline storage
-            try {
-                offlineProcessor.savePacketForOffLineUser(packet, new MsgRepositoryImpl(repo));
-            }
-            catch (UserNotFoundException e) {
-                if ( log.isLoggable( Level.FINEST ) ){
-                    log.finest("UserNotFoundException at trying to save packet for off-line user." + packet );
-                }
-            }
-
-            packet.processedBy(ID);
         }
     }
 
@@ -96,39 +74,6 @@ public class KontalkPushNotifications extends XMPPProcessor implements XMPPPostp
     @Override
     public String[] supNamespaces() {
         return XMLNSS;
-    }
-
-    private class MsgRepositoryImpl implements MsgRepositoryIfc {
-        private NonAuthUserRepository repo = null;
-
-        private MsgRepositoryImpl(NonAuthUserRepository repo) {
-            this.repo = repo;
-        }
-
-        @Override
-        public void initRepository(String conn_str, Map<String, String> map) {
-            // nothing to do here as we base on UserRepository which is already initialized
-        }
-
-        //~--- get methods --------------------------------------------------------
-        @Override
-        public Element getMessageExpired( long time, boolean delete ) {
-            throw new UnsupportedOperationException( "Not supported yet." );
-        }
-
-        //~--- methods ------------------------------------------------------------
-        @Override
-        public Queue<Element> loadMessagesToJID( JID to, boolean delete )
-                throws UserNotFoundException {
-            throw new UnsupportedOperationException( "Not supported yet." );
-        }
-
-        @Override
-        public void storeMessage( JID from, JID to, Date expired, Element msg )
-                throws UserNotFoundException {
-            repo.addOfflineDataList( to.getBareJID(), ID, "messages",
-                    new String[] { msg.toString() } );
-        }
     }
 
 }

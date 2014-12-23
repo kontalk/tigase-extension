6510cfb3364c9c599cc7bb7e9d493d80b8bd6dcb
Daniele Ricci <daniele.athome@gmail.com>
2014-12-23 20:52:19 +0100

    Install tigase-server-master too
    Signed-off-by: Daniele Ricci <daniele.athome@gmail.com>

--- install-deps.sh
+++ install-deps.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 # Installs Tigase from Kontalk custom repositories
 
-REPOS="gnupg-for-java tigase-utils tigase-server"
+REPOS="gnupg-for-java tigase-utils"
 
 for REPO in ${REPOS}; do
   echo "Building ${REPO}"
   git clone "https://github.com/kontalk/${REPO}.git" &&
   cd "${REPO}" &&
   mvn install &&
+  cd .. &&
   rm -fR "${REPO}" ||
   exit $?
 done
+
+REPO="tigase-server"
+echo "Building ${REPO}"
+git clone "https://github.com/kontalk/${REPO}.git" &&
+cd "${REPO}/modules/master" &&
+mvn install &&
+cd ../.. &&
+mvn install
+cd .. &&
+rm -fR "${REPO}" ||
+exit $?

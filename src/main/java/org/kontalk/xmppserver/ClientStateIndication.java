269fd2f885171d4475b3dc8114bddac524e46e4d
Daniele Ricci <daniele.athome@gmail.com>
2015-02-23 12:14:33 +0100

    Draft implementation of XEP-0352: CSI (#6)
    Signed-off-by: Daniele Ricci <daniele.athome@gmail.com>

---
+++ src/main/java/org/kontalk/xmppserver/ClientStateIndication.java
@@ -0,0 +1,158 @@
+/*
+ * Kontalk XMPP Tigase extension
+ * Copyright (C) 2014 Kontalk Devteam <devteam@kontalk.org>
+
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.kontalk.xmppserver;
+
+import tigase.db.NonAuthUserRepository;
+import tigase.server.Packet;
+import tigase.server.Presence;
+import tigase.xml.Element;
+import tigase.xmpp.*;
+
+import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+/**
+ * XEP-0352: Client State Indication
+ * @author Daniele Ricci
+ */
+public class ClientStateIndication extends XMPPProcessorAbstract implements XMPPPreprocessorIfc {
+    private static Logger log = Logger.getLogger(ClientStateIndication.class.getName());
+
+    private static final String XMLNS = "urn:xmpp:csi:0";
+    public static final String ID = XMLNS;
+
+    private static final String[] XMLNSS = {XMLNS, XMLNS, XMLNS};
+
+    private static final String ELEM_ACTIVE = "active";
+    private static final String ELEM_INACTIVE = "inactive";
+
+    private static final String[][] ELEMENTS = {
+            { ELEM_ACTIVE },
+            { ELEM_INACTIVE },
+    };
+
+    private static final Element[] FEATURES = { new Element("csi", new String[] { "xmlns" }, new String[] { XMLNS }) };
+
+    private static final String SESSION_QUEUE = ID + ":queue";
+
+    @Override
+    public void processFromUserToServerPacket(JID connectionId, Packet packet, XMPPResourceConnection session, NonAuthUserRepository repo, Queue<Packet> results, Map<String, Object> settings) throws PacketErrorTypeException {
+        if (packet.getElemName().equals(ELEM_ACTIVE)) {
+            deactivate(session, results);
+        }
+        else if (packet.getElemName().equals(ELEM_INACTIVE)) {
+            activate(session);
+        }
+    }
+
+    /** Activates client state indication (that is, client going to inactive state). */
+    private void activate(XMPPResourceConnection session) {
+        session.putSessionData(SESSION_QUEUE, new InternalQueue());
+    }
+
+    /** Deactivates client state indication (that is, client going to active state). */
+    private void deactivate(XMPPResourceConnection session, Queue<Packet> results) {
+        final InternalQueue queue = (InternalQueue) session.getSessionData(SESSION_QUEUE);
+        if (queue == null)
+            return;
+
+        synchronized (queue) {
+            // send all pending presence data
+            results.addAll(queue.values());
+            // destroy and remove stanza store
+            queue.clear();
+            session.removeSessionData(SESSION_QUEUE);
+        }
+    }
+
+    @Override
+    public boolean preProcess(Packet packet, XMPPResourceConnection session, NonAuthUserRepository repo, Queue<Packet> results, Map<String, Object> settings) {
+        final InternalQueue queue = (InternalQueue) session.getSessionData(SESSION_QUEUE);
+        if (queue == null)
+            return false;
+
+        try {
+            synchronized (queue) {
+                if (session.isUserId(packet.getStanzaTo().getBareJID())) {
+                    if (log.isLoggable(Level.FINEST)) {
+                        log.log(Level.FINEST, "Calling method: {0}, for packet={1}, for session={2}",
+                                new Object[]{"processToUserPacket",
+                                        packet, session});
+                    }
+                    return filterPacket(packet, queue);
+                }
+            }
+        }
+        catch (NotAuthorizedException ignored) {
+            // ignore
+        }
+
+        return false;
+    }
+
+    private boolean filterPacket(Packet packet, InternalQueue queue) {
+        if (packet.getElemName() == Presence.ELEM_NAME &&
+                (packet.getType() == null ||
+                packet.getType() == StanzaType.available ||
+                packet.getType() == StanzaType.unavailable)) {
+            queue.put(packet.getStanzaFrom(), (Presence) packet);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void processServerSessionPacket(Packet packet, XMPPResourceConnection session, NonAuthUserRepository repo, Queue<Packet> results, Map<String, Object> settings) throws PacketErrorTypeException {
+        // not used.
+    }
+
+    @Override
+    public String id() {
+        return ID;
+    }
+
+    @Override
+    public String[][] supElementNamePaths() {
+        return ELEMENTS;
+    }
+
+    @Override
+    public String[] supNamespaces() {
+        return XMLNSS;
+    }
+
+    @Override
+    public Element[] supStreamFeatures(XMPPResourceConnection session) {
+        if (log.isLoggable(Level.FINEST) && (session != null)) {
+            log.finest("VHostItem: " + session.getDomain());
+        }
+        if (session != null) {
+            return FEATURES;
+        } else {
+            return null;
+        }
+    }
+
+    /** A typedef for the internal stanza queue for CSI. */
+    private static final class InternalQueue extends LinkedHashMap<JID, Presence> {
+    }
+
+}
